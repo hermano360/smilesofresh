c36528a68eba251fde7da985159b91f42a432743
Herminio Garcia <herminio@budsy.io>
2018-03-08 22:44:49 -0800

    adding dropdown menus to all the navbar action buttons

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,12 +5,19 @@ function showDropdownElements(element) {
 function expandOrtho(){
   showDropdownElements('orthoDropdown')
 }
+function expandPed(){
+  showDropdownElements('pedDropdown')
+}
+function expandTeam(){
+  showDropdownElements('teamDropdown')
+}
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.action')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+  if (!event.target.matches('.ortho-action')) {
+
+    var dropdowns = document.getElementsByClassName("ortho-dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
@@ -19,4 +26,29 @@ window.onclick = function(event) {
       }
     }
   }
+
+  if (!event.target.matches('.ped-action')) {
+
+    var dropdowns = document.getElementsByClassName("ped-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  if (!event.target.matches('.team-action')) {
+
+    var dropdowns = document.getElementsByClassName("team-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+
 }
