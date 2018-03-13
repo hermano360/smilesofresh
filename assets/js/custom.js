e7e0f80e949ab8378da0f0113e03304b45eba070
Herminio Garcia <herminio@budsy.io>
2018-03-13 09:35:01 -0700

    dropdown changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,47 +8,35 @@ function expandOrtho(){
 function expandPed(){
   showDropdownElements('pedDropdown')
 }
-function expandTeam(){
-  showDropdownElements('teamDropdown')
+function expandAbout(){
+  showDropdownElements('aboutDropdown')
+}
+
+
+function hideSection(section){
+  var dropdowns = document.getElementsByClassName(section + "-dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
 
   if (!event.target.matches('.ortho-action')) {
-
-    var dropdowns = document.getElementsByClassName("ortho-dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    hideSection('ortho')
   }
 
   if (!event.target.matches('.ped-action')) {
-
-    var dropdowns = document.getElementsByClassName("ped-dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    hideSection('ped')
   }
 
-  if (!event.target.matches('.team-action')) {
-
-    var dropdowns = document.getElementsByClassName("team-dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+  if (!event.target.matches('.about-action')) {
+    hideSection('about')
   }
 
 }
