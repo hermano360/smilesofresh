df7932f9d469d1459d1af83c0dbde5057605da5e
Herminio Garcia <herminio@budsy.io>
2018-03-18 13:10:06 -0700

    adding photos to the different pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,3 +40,13 @@ window.onclick = function(event) {
   }
 
 }
+
+$(document).ready(function(){
+	// Set the interval to be 5 seconds
+	var t = setInterval(function(){
+		$("#carousel ul").animate({marginLeft:'-100%'},1000,function(){
+			$(this).find("li:last").after($(this).find("li:first"));
+			$(this).css({marginLeft:0});
+		})
+	},5000);
+});
